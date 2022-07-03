9790e0900eef301fc419610b08426354acdb3eba
olegtititele <96022759+olegtititele@users.noreply.github.com>
2022-07-04 00:45:59 +0300

    Update DBconfig.cs

--- DB/DBconfig.cs
+++ DB/DBconfig.cs
@@ -3,7 +3,7 @@ namespace PostgreSQL
 {
     public static class DBConfig
     {
-        public static string user_db = "Host=localhost;Username=postgres;Password=admin1305;Database=NFTbotUsers";
-        public static string nft_db = "Host=localhost;Username=postgres;Password=admin1305;Database=NFTCollectionsDB";
+        public static string user_db = "Host=89.208.137.190;Username=postgres;Password=admin1305;Database=NFTbotUsers";
+        public static string nft_db = "Host=89.208.137.190;Username=postgres;Password=admin1305;Database=NFTCollectionsDB";
     }
 }
